9d95ae30cb62585a1ad300af9f4796cb53915d5a
xiaofei9 <xiaofei9@staff.weibo.com>
2016-07-06 12:11:37 +0800

    修改表格标题

--- DailyTask.py
+++ DailyTask.py
@@ -54,7 +54,7 @@ def grabData():
 		tableinfo2['theme']=str(platform)+'影响用户数('+str(date)+')'
 		tableinfo2['title']=['序号','微博版本','影响用户数']
 		tablelist.append(tableinfo2)
-		
+
 		# 影响用户深度Top10的crash统计
 		test = EsQueryCrashInfluenceDepth.EsQueryCrashInfluenceDepth(params)
 		test.doRequest()
@@ -62,8 +62,8 @@ def grabData():
 		tableinfo={}
 		tableinfo['filepath']=filepath
 		tableinfo['sheet']=0
-		tableinfo['theme']=str(platform)+'影响用户深度Top'+str(top_number)+'的crash('+str(date)+')'
-		tableinfo['title']=['序号','uid','crash内容','影响用户数']
+		tableinfo['theme']=str(platform)+'影响用户深度Top'+str(top_number)+'的crash('+str(date)+')  单个用户单个crash的影响次数/天'
+		tableinfo['title']=['序号','uid','crash内容','crash次数']
 		tablelist.append(tableinfo)
 
 		# 抓取Top10的crash
